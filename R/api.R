631f022055e83e6648e5717e62cf5581a8234b44
MrDomani <pmorgen99@gmail.com>
2020-04-16 13:30:54 +0200

    divided into smaller functions

--- R/api.R
+++ R/api.R
@@ -15,7 +15,6 @@
 #' @export
 
 
-# Przyjmuje URL do PDFa na wejściu i konwertuje na HTML za pośrednictwem cloudconvert
 convert_pdf_2_html <- function(input_file_url, output_file = NULL, quiet = FALSE, api_key = NULL){
   input_file_name <- basename(input_file_url)
   if(file.exists(input_file_url)) stop('input_file_url must be URL to file, available online. See ?convert_pdf_to_html')
@@ -36,15 +35,39 @@ convert_pdf_2_html <- function(input_file_url, output_file = NULL, quiet = FALSE
   
   if(is.null(api_key)) api_key <- readLines(system.file('extdata/api_key.txt', package = 'CodeExtractoR'))
   base_url <- "https://api.cloudconvert.com/v1/process"
-  authorization_header <- httr::add_headers(Authorization = paste0('Bearer ',
+  auth_header <- httr::add_headers(Authorization = paste0('Bearer ',
                                                                    api_key))
   
   # 1) New process ID
   
+  req <- api_new_process(base_url, auth_header, quiet)
+  req_content <- httr::content(req)
+  process_url <- paste0('https:', req_content$url)
+  
+  # 2) New conversation
+  
+  req2 <- api_new_conversation(process_url, auth_header, input_file_url, quiet)
+  req2_content <- httr::content(req2)
+  
+  # 3) Check the status & wait for success or failure
+  
+  req3 <- api_check_status(process_url, auth_header, quiet)
+  
+  # 4) Download data
+  download_url <- paste0('https:',
+                         httr::content(req3)$output$url)
+  req4 <- api_download(download_url, auth_header, output_file, quiet)
+  
+  # 5) Write to file
+  writeBin(req4$content, con = output_file)
+  if(!quiet) message(paste0('Data saved to file', output_file))
+} 
+
+api_new_process <- function(base_url, auth_header, quiet){
   req <- httr::POST(base_url,
-              authorization_header,
-              httr::content_type_json(),
-              body = '{
+                    auth_header,
+                    httr::content_type_json(),
+                    body = '{
                         "inputformat": "pdf",
                         "outputformat": "html"
                       }')
@@ -55,24 +78,23 @@ convert_pdf_2_html <- function(input_file_url, output_file = NULL, quiet = FALSE
       httr::status_code(req)
     ),
     call. = FALSE)
-  } else message('New process initiated')
-  req_content <- httr::content(req)
-  process_url <- paste0('https:', req_content$url)
-  
-  # 2) New conversation
-  
+  } else if(!quiet) message('New process initiated')
+  req
+}
+
+api_new_conversation <- function(process_url, auth_header, input_file_url, quiet){
   req2 <- httr::POST(process_url,
-               authorization_header,
-               httr::content_type_json(),
-               body = paste0(
-                      '{
+                     auth_header,
+                     httr::content_type_json(),
+                     body = paste0(
+                       '{
                           "input": "download",
                           "file": "',
-                          input_file_url,
-                          '",
+                       input_file_url,
+                       '",
                           "outputformat": "html"
                        }'
-               ))
+                     ))
   
   if(httr::http_error(req2)){
     stop(sprintf(
@@ -80,14 +102,13 @@ convert_pdf_2_html <- function(input_file_url, output_file = NULL, quiet = FALSE
       httr::status_code(req2)
     ),
     call. = FALSE)
-  } else message('New conversation initiated')
-  
-  req2_content <- httr::content(req2)
-  
-  # Check the status & wait for success or failure
-  
+  } else if(!quiet) message('New conversation initiated')
+  req2
+}
+
+api_check_status <- function(process_url, auth_header, quiet){
   req3 <- httr::GET(paste0(process_url, '?wait'),
-                       authorization_header)
+                    auth_header)
   
   if(httr::http_error(req3)){
     stop(sprintf(
@@ -95,25 +116,19 @@ convert_pdf_2_html <- function(input_file_url, output_file = NULL, quiet = FALSE
       httr::status_code(req3)
     ),
     call. = FALSE)
-  } else message('Conversion finished. Downloading...')
-  
-  # Download data
-  download_url <- paste0('https:',
-                         httr::content(req3)$output$url)
+  } else if(!quiet) message('Conversion finished. Downloading...')
+  req3
+}
+
+api_download <- function(download_url, auth_header, output_file, quiet){
   req4 <- httr::GET(download_url,
-              authorization_header)
-  if(httr::http_error(req3)){
+                    auth_header)
+  if(httr::http_error(req4)){
     stop(sprintf(
       "Download failed [%s]", 
-      httr::status_code(req3)
+      httr::status_code(req4)
     ),
     call. = FALSE)
-  } else message(paste0('Data downloaded, writing into file ', output_file))
-  
-  # Write to file
-  writeBin(req4$content, con = output_file)
-  message(paste0('Data saved to file', output_file))
-} 
-# doc_url <- "https://journal.r-project.org/archive/2014/RJ-2014-011/RJ-2014-011.pdf"
-# api_key_v1 <- "WJMTcdHTp9NMB1JazDUDWZBU39q2UJYWx75KSFpwMZ32GhP500tuk6VVxJPh6i9R"
-# api_key2 <- "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjI2ODIzNGQ5NzY0ZDA5YWRjM2U3YjYyMTIyNDU0NGNkMWY5OGEzYTA4ODJjNDZjODg3NzJmMmI2MGM0NTc3NTgwNjg5NDBhMjY5ZmE4YzVkIn0.eyJhdWQiOiIxIiwianRpIjoiMjY4MjM0ZDk3NjRkMDlhZGMzZTdiNjIxMjI0NTQ0Y2QxZjk4YTNhMDg4MmM0NmM4ODc3MmYyYjYwYzQ1Nzc1ODA2ODk0MGEyNjlmYThjNWQiLCJpYXQiOjE1ODYxNzM5NjEsIm5iZiI6MTU4NjE3Mzk2MSwiZXhwIjo0NzQxODQ3NTYxLCJzdWIiOiI0MTM4NTY4MSIsInNjb3BlcyI6WyJ0YXNrLnJlYWQiLCJ0YXNrLndyaXRlIiwicHJlc2V0LnJlYWQiLCJwcmVzZXQud3JpdGUiXX0.eq_HHuXjJKDrP5n8WmgdivOrOBfDTNHJ06HyTCqVZfUV4sjSvm9WojFBXeDTa5IqBcPnlR5WXhBcCST4qhC-E9qkuZ6bAE1EFXRfKhuHlie4vP8Pe4Fpe1jxJmLC_3lPOgv6FgKjSr-wCp4hnFOJwFo2Y2SiDyEmtDiVz4qKY2EGzIBQH1CHkeNGcw9K_Arg1LMXN7E_uFoOTqqrtgxDYn8mSUlunQwNMesyznnLX9g27-yWalYVvQm3Ld0q_HfDNRAoIpUWA8xE308pVC4Hu7AueSL-yCNGQurS2Smh8-_kSq7ErpvcG16lv26MzxxZsum0DpsVrA8cDbN8qbk5USFiGJgdhWFSsCSQKiwF1jVqP82_VICp0Vb9BwvYGlSVbMQGov1dT6bwZfN5eTJli-FT_F1Sm7GEiZP4rzAxKANw28iu7l8aDhkdfzil7j93rgvguV4W2ppkSYDVksqOVGv9q5XkdHUsuwOyu7wTz7nIsY_ua3V65tqBB0AioXPogK0f8qBoitPw8apddtzTinCIS-vtAafVzQ5UtcQrLo8WsQP2Pqp1eaDk4S0r0kpBNgWtzLpDE1GRGE6AJak0j1Tnmcy4vZrUhvumuOUommop-KNLg4wkbI675STwxoB-YM0oXym-gxKNtGQf48020QiO3Vr4PyXPEggHZd2Sd18"
+  } else if(!quiet) message(paste0('Data downloaded, writing into file ', output_file))
+  req4
+}
